c78f22acc748757d964d1a06c55a6e224ed669b9
tophy <tophy@radishfiction.com>
2022-08-11 19:39:34 +0900

    chore: apply 4-tab

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,49 +1,49 @@
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Button } from 'react-native-material-buttons';
+import React, {PureComponent} from 'react';
+import {Button} from 'react-native-material-buttons';
 
 import styles from './styles';
 
 export default class DropdownItem extends PureComponent {
-  static defaultProps = {
-    color: 'transparent',
-    disabledColor: 'transparent',
-    rippleContainerBorderRadius: 0,
-    shadeBorderRadius: 0,
-  };
+    static defaultProps = {
+        color: 'transparent',
+        disabledColor: 'transparent',
+        rippleContainerBorderRadius: 0,
+        shadeBorderRadius: 0,
+    };
 
-  static propTypes = {
-    ...Button.propTypes,
+    static propTypes = {
+        ...Button.propTypes,
 
-    index: PropTypes.number.isRequired,
-  };
+        index: PropTypes.number.isRequired,
+    };
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.onPress = this.onPress.bind(this);
-  }
-
-  onPress() {
-    let { onPress, index } = this.props;
-
-    if ('function' === typeof onPress) {
-      onPress(index);
+        this.onPress = this.onPress.bind(this);
     }
-  }
 
-  render() {
-    let { children, style, index, ...props } = this.props;
+    onPress() {
+        let {onPress, index} = this.props;
 
-    return (
-      <Button
-        {...props}
+        if ('function' === typeof onPress) {
+            onPress(index);
+        }
+    }
 
-        style={[styles.container, style]}
-        onPress={this.onPress}
-      >
-        {children}
-      </Button>
-    );
-  }
+    render() {
+        let {children, style, index, ...props} = this.props;
+
+        return (
+            <Button
+                {...props}
+
+                style={[styles.container, style]}
+                onPress={this.onPress}
+            >
+                {children}
+            </Button>
+        );
+    }
 }
